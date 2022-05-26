fc0d61c45cd15718c0cd99c2875a7c1058ebad8a
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-05-27 02:17:31 +0530

    hamburger fixed

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Landing = () => {
   return (
     <>
-      <div className="p-4 bg-[#004445] flex flex-row rounded-b-2xl">
+      <div className="py-4 px-8 bg-[#004445] flex flex-row rounded-b-2xl">
         <h1 className="text-3xl tracking-wide font-semibold text-[#DDF0A7]">
           CPMC
         </h1>
 
-        <div className="hamburger ml-auto">Hamburger here</div>
+        <div className="hamburger ml-auto justify-center items-center flex flex-col  space-y-4">
+          <div className="bg-[#fff] w-14 h-[5px] "></div>
+          <div className="bg-[#fff] w-8 h-[5px] "></div>
+          <div className="bg-[#fff] w-14 h-[5px] "></div>
+        </div>
       </div>
       <div className="font-semibold text-center align-middle justify-center h-screen  bg-green-50 ">
         <h1 className="text-[#469697] pt-32 tracking-wide text-3xl lg:text-5xl pb-5">
@@ -23,6 +27,6 @@ const Landing = () => {
       </div>
     </>
   );
-}
+};
 
-export default Landing
+export default Landing;
