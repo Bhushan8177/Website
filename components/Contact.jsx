c0ff73968fec69c903c7df1675635a3b1ea53485
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-27 02:23:27 +0530

    added contact form skeleton

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,6 +13,16 @@ const Contact = () => {
         <div className="pl-8 m-2 rounded-full p-0.5 bg-[#004445]"></div>
 
     </div>
+    <div className="flex flex-col pl-4 pr-4">
+        <h1 className="font-semibold text-2xl text-[#469697]">Messsage us</h1>
+        
+
+        <input type="text" placeholder='name' className='p-4 border-2 rounded-xl mb-2 mt-4' />
+        <input type="text" placeholder='email' className='p-4 border-2 rounded-xl mb-2 ' />
+       
+        <input type="text" placeholder='contact' className='p-4 border-2 rounded-xl mb-2' />
+        <textarea name="message" placeholder='message...' cols="30" rows="10" className='p-4 border-2 rounded-xl'></textarea>
+    </div>
     </div>
     </>
   )
