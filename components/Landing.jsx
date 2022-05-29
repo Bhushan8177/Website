29e2ba5c7314cc168274779e256e16d1f0d9ea91
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-29 19:19:11 +0530

    Added Responsive Renders in Landing Page

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,4 +1,4 @@
-import React,{Suspense} from "react";
+import React, { Suspense } from "react";
 import Image from "next/image";
 import downArrow from "./assets/downArrow.svg";
 import a from "./assets/a.png";
@@ -10,22 +10,11 @@ import v3 from "./assets/v3.png";
 import { AmbientLight } from "three";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Box from './threejs/Box'
+import Box from "./threejs/Box";
 const Landing = () => {
   return (
     <>
       <div className="py-4 px-8 bg-[#004445] flex flex-row ">
-      <div className="inline-block w-[100px] h-[100px] float-right cursor-pointer">
-        <Canvas className="">
-        <OrbitControls enableDamping={true} enableZoom={false}/>
-        <ambientLight intensity={0.5}/>
-        <directionalLight position={[-2,5,2]} intensity={2}/>
-        <Suspense fallback={null}>
-
-        <Box/>
-        </Suspense>
-      </Canvas>
-        </div>
         <div className="hamburger ml-auto justify-center cursor-pointer items-center flex flex-col  space-y-2.5">
           <div className="bg-[#fff] w-14 h-[4px] "></div>
           <div className="bg-[#fff] w-8 h-[4px] "></div>
@@ -33,59 +22,67 @@ const Landing = () => {
         </div>
       </div>
       <div className="font-semibold text-center align-middle justify-center h-[70vh] bg-[#004445] rounded-b-[16%]">
-        
-
-        
         <h1 className="text-[#469697] pt-32 tracking-wide text-3xl lg:text-5xl pb-5">
           Competetive Programming
         </h1>
-        
-     
+
         <h1 className="text-[#EDEDED]  tracking-wide text-3xl lg:text-5xl pb-5">
           and MentorShip Club
         </h1>
         <h1 className="text-[#469697]  tracking-wide text-3xl lg:text-5xl">
           DYPCOE<span className="text-[#EDEDED]">, Pune.</span>
-          
         </h1>
-        
-       
       </div>
-      <div className="mt-[150px] px-40">
-        <div className="flex items-center justify-between">
-          <div className="">
-            <Image src={a} alt="down" width={"500px"} height={"500px"} />
+
+      <div className="flex flex-col lg:ml-64 lg:mr-64 pb-12 mt-8 ml-2 mr-2">
+        <div className="flex flex-row mb-8">
+          <div className="inline-block w-[200px] h-[200px] float-right cursor-pointer">
+            <Canvas className="">
+              <OrbitControls enableDamping={true} enableZoom={false} />
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[-2, 5, 2]} intensity={2} />
+              <Suspense fallback={null}>
+                <Box />
+              </Suspense>
+            </Canvas>
           </div>
-          {/* <div className=" border-dotted border-4 h-[500px]  border-[#8FC7C8]"></div> */}
-          <div className="text-[#469697] font-Paytone font-[800] leading-[67px] text-[48px]">
-            Improve <br /> problem <br />
-            solving skills
-          </div>
-        </div>
-        <div className="flex items-center justify-between">
-          {/* <div className=" border-dotted border-4 h-[500px]  border-[#8FC7C8]"></div> */}
-          <div className="text-[#469697] font-Paytone font-[800] leading-[67px] text-[48px]">
-            Group disccusion
-            <br /> on Competitive <br />
-            programming
-          </div>
-          <div className="">
-            <Image src={b} alt="down" width={"500px"} height={"400px"} />
+          <div className="text-[#469697] lg:text-4xl text-2xl font-bold lg:ml-[18rem] lg:pt-20 pt-8  ">
+            Improve problem solving skills
           </div>
         </div>
-        <div className="flex items-center justify-between">
-          <div className="">
-            <Image src={c} alt="down" width={"500px"} height={"500px"} />
+        <div className="flex flex-row mb-8">
+          <div className="text-[#469697] lg:text-4xl text-2xl font-bold lg:mr-[20rem] text-left pl-2 justify-left items-left lg:pt-20 pt-8 ">
+            Group disccusion on Competitive programming
           </div>
-          {/* <div className=" border-dotted border-4 h-[500px]  border-[#8FC7C8]"></div> */}
-          <div className="text-[#469697] font-Paytone font-[800] leading-[67px] text-[48px] ">
-            Helping each other
-            <br /> to learn new <br />
-            technical skill
+          <div className="inline-block w-[200px] h-[200px]  cursor-pointer">
+            <Canvas className="">
+              <OrbitControls enableDamping={true} enableZoom={false} />
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[-2, 5, 2]} intensity={2} />
+              <Suspense fallback={null}>
+                <Box />
+              </Suspense>
+            </Canvas>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="inline-block w-[200px] h-[200px] float-right cursor-pointer">
+            <Canvas className="">
+              <OrbitControls enableDamping={true} enableZoom={false} />
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[-2, 5, 2]} intensity={2} />
+              <Suspense fallback={null}>
+                <Box />
+              </Suspense>
+            </Canvas>
+          </div>
+          <div className="text-[#469697] lg:text-4xl text-2xl font-bold lg:ml-[18rem] lg:pt-20 pt-8">
+            Helping each other to learn new technical skill
           </div>
         </div>
       </div>
-      <section className="vision_section ">
+
+       <section className="vision_section ">
         <div className="text-center pt-[50px] font-Paytone font-[800] text-[48px] text-[#fff]">
           OUR VISION
         </div>
@@ -141,7 +138,7 @@ const Landing = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> 
     </>
   );
 };
