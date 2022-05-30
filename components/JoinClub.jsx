91500cc04c67009fe48b5bfe3c2f7e5144a8e649
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-31 01:16:43 +0530

    Join Club 🚀

---
+++ components/JoinClub.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from "../components/LandingSlide/LandingSlide.module.css";
+
+
+const JoinClub = () => {
+  return (
+    <div className="mt-16 mb-16">
+        <div className="font-bold lg:text-5xl pb-4 text-3xl text-center justify-center ">
+            
+            <h1 className='text-[#A8ECF0]'>
+                <span className='text-[#FFFFFF]'>WANT TO </span>
+                JOIN CLUB 
+                <span className='text-[#FFFFFF]'> ?</span>    
+            </h1>
+        </div>
+            <div className="text-sm pb-[1.5rem]  flex flex-row items-center justify-center break-words font-normal text-center text-[#FFFFFF]  pl-8 pr-8  lg:h-[64px]">
+            Our club seeks to increase students' problem-solving skills as well as their cp in order to improve their coding abilities. The following three steps describe how our Club operates.
+            </div>
+            <button className={classes.btn}>
+          <span className={classes.span}>JOIN NOW</span>
+          <svg viewBox="0 0 13 10" height="10px" width="15px">
+            <path d="M1,5 L11,5"></path>
+            <polyline points="8 1 12 5 8 9"></polyline>
+          </svg>
+        </button>
+    </div>
+  )
+}
+
+export default JoinClub
--- pages/index.jsx
+++ pages/index.jsx
@@ -17,6 +17,7 @@ import {
   Register,
   Work,
   Vision,
+  JoinClub
 } from "../Components";
 
 export default function Home() {
@@ -32,6 +33,7 @@ export default function Home() {
       <Timeline />
       {/* <Vision /> */}
       <Mission />
+      <JoinClub/>
       <Work />
       <Register />
       <Footer />
