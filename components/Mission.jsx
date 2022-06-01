b9e9c32f495cb01910e5de1ededc3578e8064b20
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-06-01 20:18:31 +0530

    timeline and mission responsive

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import  Image  from "next/image";
-import strongCommunity from './assets/StrongCommunity.svg'
-import competativeProgramming from './assets/CompitativeProgramming.svg'
-import mentoring from './assets/Mentoring.svg'
+import Image from "next/image";
+import strongCommunity from "./assets/StrongCommunity.svg";
+import competativeProgramming from "./assets/CompitativeProgramming.svg";
+import mentoring from "./assets/Mentoring.svg";
 
 const Mission = () => {
   return (
     <div className="lg:flex lg:flex-row bg-[#091E32] pt-20 pb-16">
       <div className="flex flex-col lg:pl-16 lg:pr-16 lg:mt-[180px]">
-          <div className="pl-8 pr-8">
-              
-        <h2 className="text-sm font-semibold text-[#89BAEE]  text-center lg:text-left lg:pl-0">
-          Why are we Doing This
-        </h2>
-        <h1 className="font-bold lg:text-5xl text-3xl text-[#A8ECF0] text-center lg:text-left lg:pl-0 pb-8 ">
-          <span className="text-white font-bold pr-4">OUR</span>
-          MISSION
-        </h1>
-          </div>
-        <p className="lg:text-lg text-sm lg:font-semibold font-[400] text-[#FFFFFF] pl-8 pr-8 lg:text-left text-center">
+        <div className="pl-8 pr-8">
+          <h2 className="text-sm font-semibold text-[#89BAEE]  text-center lg:text-left lg:pl-0">
+            Why are we Doing This
+          </h2>
+          <h1 className="font-bold lg:text-5xl text-3xl text-[#A8ECF0] text-center lg:text-left lg:pl-0 pb-8 ">
+            <span className="text-white font-bold pr-4">OUR</span>
+            MISSION
+          </h1>
+        </div>
+        <p className="lg:text-lg text-sm  lg:font-semibold font-[600] text-[#FFFFFF] pl-8 pr-8 lg:text-left text-center">
           Our primary goal is to promote CP in our college community and help
           students enhance their problem-solving abilities. Our primary goal is
           to promote CP in our college community and help students enhance their
           problem-solving abilities. Our primary goal is to promote CP in our
-          college community and help students enhance their
-          problem-solving abilities. Our primary goal is to promote CP in our
           college community and help students enhance their problem-solving
-          abilities.
+          abilities. Our primary goal is to promote CP in our college community
+          and help students enhance their problem-solving abilities.
         </p>
       </div>
       <div className="lg:container  lg:flex lg:flex-row">
@@ -34,45 +32,50 @@ const Mission = () => {
           <div className="lg:mb-4 rounded-2xl lg:mt-4 bg-[#0F263A] lg:p-2 lg:w-[346px] lg:h-[296px] ml-12 mr-12 mt-8 mb-4 ">
             {/* <span className="text-2xl text-[#EDEDED] font-bold">01.</span> */}
             <div className="flex justify-center items-center">
-            <Image src={strongCommunity}/>
+              <Image src={strongCommunity} />
             </div>
-           <div className="text-2xl font-semibold text-center text-[#95EBF0]">
-           Strong Community
-           </div>
-       
+            <div className="text-2xl font-semibold text-center text-[#95EBF0]">
+              Strong Community
+            </div>
+
             <h2 className="text-sm p-4  text-[#EDEDED] font-semibold tracking-tight leading-normal text-center">
-            We also want to build a robust and influential network that will assist juniors in connecting with and learning from their mentors, and seniors in preparing for competitive programming so that they may pass their interviews and land fantastic MNC jobs.
+              We also want to build a robust and influential network that will
+              assist juniors in connecting with and learning from their mentors,
+              and seniors in preparing for competitive programming so that they
+              may pass their interviews and land fantastic MNC jobs.
             </h2>
           </div>
           <div className="lg:mb-4 rounded-xl lg:mt-8 bg-[#0F263A] lg:p-2 ml-12 mr-12 mt-4 mb-3">
             {/* <span className="text-2xl text-[#EDEDED] font-bold">01.</span> */}
             <div className="flex justify-center items-center">
-            <Image src={mentoring}/>
+              <Image src={mentoring} />
             </div>
-           <div className="text-2xl font-semibold text-center text-[#95EBF0]">
-           Mentorship
-           </div>
-       
+            <div className="text-2xl font-semibold text-center text-[#95EBF0]">
+              Mentorship
+            </div>
+
             <h2 className="text-sm p-4  text-[#EDEDED] font-semibold tracking-tight leading-normal text-center">
-            Our secondary goal is to build a strong mentoring relationship between our seasoned seniors and juniors. This will allow the students to learn a lot in a short amount of time while also allowing them to form strong bonds with their peers.
+              Our secondary goal is to build a strong mentoring relationship
+              between our seasoned seniors and juniors. This will allow the
+              students to learn a lot in a short amount of time while also
+              allowing them to form strong bonds with their peers.
             </h2>
           </div>
         </div>
         <div className="lg:mb-4 rounded-xl  bg-[#0F263A] lg:p-2 lg:w-auto lg:h-[50%] lg:pb-0 lg:mt-[180px] lg:ml-0  ml-[44px] mt-6 mr-[44px] lg:mr-[100px]  ">
-         
-            <div className="flex lg:pl-8 lg:pr-8 justify-center items-center">
-            <Image src={competativeProgramming}/>
-            </div>
-           <div className="text-2xl lg:pl-8 lg:pr-8 font-semibold text-center text-[#95EBF0]">
-           Competitive Programming
-           </div>
-            <div className="lg:w-auto">
-
-            <h2 className="text-sm p-4  text-[#EDEDED] font-semibold tracking-tight leading-normal text-center">
-            Our primary goal is to promote CP in our college community and help students enhance their problem-solving abilities.
-            </h2>
-            </div>
+          <div className="flex lg:pl-8 lg:pr-8 justify-center items-center">
+            <Image src={competativeProgramming} />
           </div>
+          <div className="text-2xl lg:pl-8 lg:pr-8 font-semibold text-center text-[#95EBF0]">
+            Competitive Programming
+          </div>
+          <div className="lg:w-auto">
+            <h2 className="text-sm p-4  text-[#EDEDED] font-semibold tracking-tight leading-normal text-center">
+              Our primary goal is to promote CP in our college community and
+              help students enhance their problem-solving abilities.
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -5,30 +5,30 @@ import c from "./assets/c.png";
 import v1 from "./assets/v1.gif";
 import v2 from "./assets/v2.gif";
 import v3 from "./assets/v3.png";
-// import { AmbientLight } from "three";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls } from "@react-three/drei";
-// import Box from "./threejs/Box";
+import { AmbientLight } from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Box from "./threejs/Box";
 import logo from "./assets/CPMCLogo.svg";
 
 const Timeline = () => {
   return (
     <>
-      <div className="w-[100vw] flex flex-col bg-[#0F263A]  ">
+      <div className="w-[100vw] flex flex-col bg-[#0F263A] mt-[200px]  ">
         <div className="flex flex-row mb-8 justify-center">
           <div className=" w-[150px] h-[150px] lg:w-[350px] lg:h-[300px] flex md:mr-auto lg:mr-auto ml-auto cursor-pointer">
-            {/* <Canvas className="">
+            <Canvas className="">
               <OrbitControls enableDamping={true} enableZoom={false} />
               <ambientLight intensity={0.5} />
               <directionalLight position={[-2, 5, 2]} intensity={2} />
               <Suspense fallback={null}>
                 <Box />
               </Suspense>
-            </Canvas> */}
+            </Canvas>
           </div>
-          <div className="text-[#A8ECF0] lg:text-4xl text-2xl font-bold flex flex-col lg:pt-8 pt-8 w-[300px] mr-auto lg:pr-0 pr-8">
-            <span className="text-2xl text-[#EDEDED] font-bold">01.</span>
-            <br />
+          <div className="text-[#A8ECF0] text-right lg:text-4xl  text-2xl font-bold flex flex-col items-end lg:pt-8 pt-8 w-[300px] mr-auto lg:pr-0 pr-8">
+            <span className="text-2xl text-[#EDEDED]  font-bold">01.</span>
+            {/* <br /> */}
             IMPROVE PROBLEM SOLVING
             <br />
             <h2 className="text-sm pt-4  text-[#EDEDED] font-semibold tracking-tight leading-normal">
@@ -40,7 +40,7 @@ const Timeline = () => {
         <div className="flex flex-row mb-8 justify-center">
           <div className="text-[#A8ECF0] lg:text-4xl text-2xl font-bold flex flex-col lg:pt-8 pt-8 w-[300px] ml-auto lg:pl-0 pl-8 ">
             <span className="text-2xl text-[#EDEDED] font-bold">02.</span>
-            <br />
+            {/* <br /> */}
             LEARN AND DISCUSS <br /> COMPETITIVE PROGRAMMING
             <br />
             <h2 className="text-sm pt-4  text-[#EDEDED] font-semibold tracking-tight leading-normal">
@@ -49,30 +49,30 @@ const Timeline = () => {
             </h2>
           </div>
           <div className=" w-[150px] h-[150px] lg:w-[350px] lg:h-[300px] flex md:mr-auto flex-col lg:mr-auto ml-auto mr-auto cursor-pointer">
-            {/* <Canvas className="">
+            <Canvas className="">
               <OrbitControls enableDamping={true} enableZoom={false} />
               <ambientLight intensity={0.5} />
               <directionalLight position={[-2, 5, 2]} intensity={2} />
               <Suspense fallback={null}>
                 <Box />
               </Suspense>
-            </Canvas> */}
+            </Canvas>
           </div>
         </div>
         <div className="flex flex-row mb-8 justify-center">
           <div className=" w-[150px] h-[150px] lg:w-[350px] lg:h-[300px] flex md:mr-auto lg:mr-auto ml-auto cursor-pointer">
-            {/* <Canvas className="">
+            <Canvas className="">
               <OrbitControls enableDamping={true} enableZoom={false} />
               <ambientLight intensity={0.5} />
               <directionalLight position={[-2, 5, 2]} intensity={2} />
               <Suspense fallback={null}>
                 <Box />
               </Suspense>
-            </Canvas> */}
+            </Canvas>
           </div>
-          <div className="text-[#A8ECF0] lg:text-4xl text-2xl font-bold flex flex-col  pt-8 w-[300px] mr-auto lg:pr-0 pr-8">
+          <div className="text-[#A8ECF0] text-right lg:text-4xl text-2xl font-bold flex flex-col  pt-8 w-[300px] mr-auto lg:pr-0 pr-8">
             <span className="text-2xl text-[#EDEDED] font-bold ">03.</span>
-            <br />
+            {/* <br /> */}
             MENTORING AND HELPING EACH OTHER TO LEARN NEW SKILLS
             <br />
             <h2 className="text-sm pt-4  text-[#EDEDED] font-semibold tracking-tight leading-normal">
